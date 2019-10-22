030fcd384df3bf8f182a07a0c80219c9f7f6c709
peteraflores <Japanboysushi@gmail.com>
2019-10-22 19:45:17 -0400

    forgot to commit something. Oopsies

---
+++ src/controllers/drinks.js
@@ -0,0 +1,23 @@
+const {SQLCONNECT} = require('../sql/SQLConnect');
+
+module.exports = {
+    GUESTS: function(){
+        const connection = SQLCONNECT();
+        return new Promise((success, failure) => {
+            connection.query(`SELECT * FROM drinks WHERE available = TRUE`, 
+            (error, result)=>{
+                if (error){
+                    return failure(error);
+                }
+                return success(result.map(value => {
+                    var data = {};
+                    for(key in value){
+                        data[key] = value[key];
+                    }
+                    return data;
+                }));
+            })
+            connection.end();
+        })
+    }
+}
