40b41e72314e9ea4d80eea46c2b98c19d6577c05
peteraflores <Japanboysushi@gmail.com>
2019-10-22 19:39:21 -0400

    Defined more functionality for clients

--- src/sql/ABTM.sql
+++ src/sql/ABTM.sql
@@ -6,10 +6,17 @@ CREATE TABLE IF NOT EXISTS drinks (
   PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXISTS  guests(
+CREATE TABLE IF NOT EXISTS guests(
   id char(36) NOT NULL,
   name char(255) NOT NULL,
   venmo char(255) NOT NULL,
   drinkCount int(16) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS orders(
+  id char(36) NOT NULL,
+  name char(255) NOT NULL,
+  drink char(255) NOT NULL,
   PRIMARY KEY (id)
 );
