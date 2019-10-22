6a736e7a0f9947ce12cd3c7ed3fcf8ba795c5b2d
River Marks <rmarks6767@gmail.com>
2019-10-22 18:19:22 -0400

    Fixed the sql syntax

--- src/sql/ABTM.sql
+++ src/sql/ABTM.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS drinks (
   id char(36) NOT NULL,
   name varchar(256) NOT NULL,
-  desc varchar(256) NOT NULL,
+  description varchar(256) NOT NULL,
   available boolean NOT NULL,
   PRIMARY KEY (id)
 );
@@ -10,6 +10,6 @@ CREATE TABLE IF NOT EXISTS  guests(
   id char(36) NOT NULL,
   name char(256) NOT NULL,
   venmo char(256) NOT NULL,
-  count int(16) NOT NULL,
+  drinkCount int(16) NOT NULL,
   PRIMARY KEY (id)
 );
