0c4e7a2b7deffb80e734a7f09bb3993bb4d43dc3
River Marks <rmarks6767@gmail.com>
2019-10-26 15:59:27 -0400

    Added sorting for the queue

--- src/controllers/queue.js
+++ src/controllers/queue.js
@@ -1,5 +1,5 @@
 const {SELECT} = require('../sql/dynamicSQLSelect');
 
 module.exports = {
-    QUEUE: async (req, res) => res.json(await SELECT("orders", "fulfilled = FALSE"))
+    QUEUE: async (req, res) => res.json(await SELECT("orders", "fulfilled = FALSE ORDER BY orderDate"))
 }
